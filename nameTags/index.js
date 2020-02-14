7b00497f83c1a4cb10f62db1a0ad265000a4c127
Joaquin Vargas <joaquin.vargas90@gmail.com>
2020-02-13 18:53:21 -0600

    Name Tags

--- nameTags/index.js
+++ nameTags/index.js
@@ -6,4 +6,12 @@ subscribe(() => render(getState()));
 dispatch({ type: null }); // Here we're making a call to dispatch() - this triggers the first render.
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
+const input = document.getElementById('nameForm');
+input.addEventListener('submit', e => {
+    e.preventDefault();
 
+    let nameInputElement = document.getElementById('nameInput');
+         dispatch({ type: "ADD_NAME", name: nameInputElement.value});
+         nameInputElement.value = "";
+        }
+    )
--- nameTags/reducer.js
+++ nameTags/reducer.js
@@ -3,11 +3,17 @@ const initialState = [
     "Stuart"
 ];
 
+
 const reducer = (state = initialState, action) => {
     // Handle actions here - make sure you don't mutate the state!
     const { type } = action;
-
     // Add a Name Tag
-
-    return state;
+    if (type === "ADD_NAME"){
+        // make a copy of the state and add a circle to that copy
+        newState = state.concat(action.name);
+        console.log("returning");
+        console.log(newState);
+        return newState;
+    }
+        return state;
 }
